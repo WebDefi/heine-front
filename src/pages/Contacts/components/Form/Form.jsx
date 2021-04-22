a598eb41a26ca9e0eda285ef7cf4fab1aa974575
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-04-22 13:21:29 +0300

    +

---
+++ src/pages/Contacts/components/Form/Form.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Grid, Typography, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Input from './components/Input'
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Select from './components/Select'
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+
+export default function Form({ 
+  size = 6, 
+  children,
+  title,
+  })  {
+    
+   
+ 
+  return (
+    <Grid item xs={size}>
+      <div>
+      <Typography variant="h4">{title}</Typography>
+      <form>
+        <Grid container justify="space-between" direction="row" spacing={5}>
+          <Input placeholder="Компания" article="Компания"/> 
+          <Input placeholder="Имя" article="Имя*"/> 
+          <Input placeholder="Город" article="Город*"/> 
+          <Input placeholder="Страна" article="Страна*"/> 
+          <Input placeholder="E-Мейл" article="Е-Мейл*"/> 
+          
+          <Select article="Cпециальность"/>
+          <Select article="Функция"/>
+          <Input placeholder="Тема" article="Тема"/> 
+        </Grid>
+      </form> 
+         {children}
+      </div>
+    </Grid>
+  );
+};
---
+++ src/pages/Contacts/components/Form/components/Select.jsx
@@ -0,0 +1,51 @@
+﻿import React from 'react';
+import { Grid, Typography, TextField, Select } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+ 
+
+export default function Input({ 
+    size = 6, 
+    article
+    })  {
+        const [state, setState] = React.useState({
+            age: '',
+            name: 'hai',
+          });
+        
+          const handleChange = (event) => {
+            const name = event.target.name;
+            setState({
+              ...state,
+              [name]: event.target.value,
+            });
+          };
+   
+    return (
+        <Grid item xs={size}>
+            <Typography variant="subtitle2">{article}</Typography>
+            <FormControl variant="outlined"  size="small" fullWidth >
+
+            <Select
+              native
+              fullWidth
+              value={state.age}
+              onChange={handleChange}
+              inputProps={{
+                name: 'age',
+                id: 'outlined-age-native-simple',
+              }}>
+
+              <option aria-label="sss" value="ыглф" />
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+            </Select>
+          </FormControl>
+          </Grid>
+    );
+  };
+         
+       
+       
+       
+       
