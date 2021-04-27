12d34e95f0169a713e85750ccb974e18bbe36ae0
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-04-27 19:16:59 +0300

    Обновил сервисы

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Grid, Link, Container } from '@material-ui/core';
+import { Grid, Link, Container,Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from './components/Card/Card'
 import serviceLeft from '../../assets/images/serviceLeft.svg'
 import serviceRight from '../../assets/images/serviceRight.svg'
 import Icon from './components/svg/Icon'
+import SubHeader from '../../common/SubHeader/SubHeader'
 
 const useStyles = makeStyles(theme => ({
     leftImg: {
@@ -16,33 +17,35 @@ const useStyles = makeStyles(theme => ({
         position: "absolute",
         bottom:0,
         right:0,
-        "&:hover": {
-            stroke: "#fff",
-            fill:"#fff",
-          }
     },
  }));
 
-export default function Products(isBlack)  {
+export default function Products()  {
 const classes = useStyles();
   return (
     <div>
+    <SubHeader />
+        <div style={{background:"#fff"}}>
         <Container fluid  >
-            <Grid container justify="space-around"   spacing={6}>
-            <Icon />
-            <Card size={4} title="asdasdd"  subtitle="sussssssssss" >
-                    <Icon />
-                </Card>
-                <Card size={4} title="asdasdd"  subtitle="sussssssssss" >
+        <Typography variant="h6" style={{fontWeight:"500", paddingTop: 50}} >Мы стремимся предоставить вам самые лучшие продукты и услуги.</Typography>
+        <Typography variant="body1" style={{paddingTop: 10,paddingBottom: 50}}>Наше стремление поставлять продукцию высокого качества также включает в себя исключительный уровень обслуживания и поддержки. Предоставление надежных советов и несложной помощи - это то, что ценят наши клиенты во всем мире.</Typography>
+            <Grid container justify="space-around"   spacing={6} style={{paddingBottom:50}}>
+               
+                <Card size={4} title="ОБЩИЕ ВОПРОСЫ"  subtitle="Эти часто задаваемые вопросы разработаны, чтобы помочь вам разобраться в общих процессах и структурах, связанных с HEINE, например, «Как найти партнера по распространению»." >
                     <img className={classes.leftImg} src={serviceLeft}></img>
                     <img className={classes.rightImg} src={serviceRight}></img>
                 </Card>
-                <Card size={4} title="asdasdd"  subtitle="sussssssssss" >
+                <Card size={4} title="ЗАГРУЗКИ"  subtitle="Эти часто задаваемые вопросы разработаны, чтобы помочь вам разобраться в общих процессах и структурах, связанных с HEINE, например, «Как найти партнера по распространению»." >
+                    <img className={classes.leftImg} src={serviceLeft}></img>
+                    <img className={classes.rightImg} src={serviceRight}></img>
+                </Card>
+                <Card size={4} title="ВИДЕО КОНТЕНТ"  subtitle="Эти часто задаваемые вопросы разработаны, чтобы помочь вам разобраться в общих процессах и структурах, связанных с HEINE, например, «Как найти партнера по распространению»." >
                     <img className={classes.leftImg} src={serviceLeft}></img>
                     <img className={classes.rightImg} src={serviceRight}></img>
                 </Card>
             </Grid>
         </Container>
+        </div>
     </div>
   );
 };
