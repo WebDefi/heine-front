963845e7dc7d8943e8f59add3806649954eb64a9
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-05-10 17:43:21 +0300

    Добавил все компоненты на страницу

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -5,6 +5,7 @@ import Info from './components/Info/Info'
 import Adress from './components/Info/components/Adress/Adress'
 import SubHeader from '../../common/SubHeader/SubHeader'
 import Form from '../Contacts/components/Form/Form'
+import News from './components/News/News'
 export default function Products()  {
  
   return (
@@ -19,11 +20,12 @@ export default function Products()  {
                         <li>Тел.: +49 8105 7728 0</li>
                     </ul>
                 </Adress>
-                <Form title="Adasdasdasd" size={12}>
-                    
-                </Form>
+                <Form title="Adasdasdasd" size={12}/>
+                <News size="12" title="asdasd"/>
             </Grid>
+
         </Container>
+        <iframe style={{border:"none",marginTop:80}} src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d22050.16573887866!2d30.661268899999996!3d46.3044608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1620511658488!5m2!1sru!2sua" width="100%" height="450" border="none" allowfullscreen="" loading="lazy"></iframe>
     </div>
   );
 };
