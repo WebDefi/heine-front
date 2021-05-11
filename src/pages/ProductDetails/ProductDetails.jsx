f1b7d3aac966b767e34fdf3faa68ffca8304a8fe
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-05-11 16:39:59 +0300

    Начинаю работу над ProductDetails component

---
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { Grid, Link, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import SubHeader from '../../common/SubHeader/SubHeader'
+
+const useStyles = makeStyles(theme => ({
+  productsSection: {
+    background:theme.palette.white.main,
+    padding:"70px 0"
+  }
+
+}));
+
+export default function ProductDetails()  {
+  const classes = useStyles();
+  return (
+    <div>
+      <SubHeader /> 
+      <div className={classes.productsSection}>
+          <Container fluid >
+              <Grid container>
+                  
+              </Grid>
+          </Container>
+      </div>
+    </div>
+  );
+};
