26b32b3e0395ecc68db079672494c214de0750ee
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-06-10 11:12:33 +0300

    Card component updated

--- src/pages/Service/components/Card/Card.jsx
+++ src/pages/Service/components/Card/Card.jsx
@@ -1,39 +1,8 @@
 import React from 'react';
 import { Grid, Container, Typography, Paper, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
- 
-const useStyles = makeStyles(theme => ({
-   categoryBlock: {
-        borderRadius: "0 20px 0 0",
-        height:230,
-        position: "relative",
-        display:"flex",
-        justifyContent:"center",
-        alignItems: "center",
-        
-        overflow: "hidden",
-        marginTop: theme.spacing(1),
-        background:theme.palette.secondary.main,
-        
-        border:theme.palette.secondary.secondaryBorder,
-        "&:hover": {
-            border:theme.palette.mainBtnBorder.main,
-            background:theme.palette.primary.main,
-            
-          }
-   },
-    
-   categoryContentText: {
-    margin:"10px 20px",
-    '&:hover':{
-      color: theme.palette.primary.main,
+import './Card.css'
 
-    },
-        
-   },
-  
-
-}));
 
 export default function Card({ 
   size = 6, 
@@ -44,22 +13,23 @@ export default function Card({
   article,
   link,
   })  {
-  const classes = useStyles();
   return (
  
  
         <Grid item xs={size}>
-            <Link to="{link}" href="{link}">
-                <Paper className={classes.categoryBlock}>
-                {children}
-                <Typography variant="h2" style={{position:"absolute",color:"#fff", margin:'2rem'}}>{title}</Typography>
-                </Paper>
-                <div style={{border:"1px solid #444"}}>
-                  <Typography variant="subtitle1" className={classes.categoryContentText} >{title}</Typography>
-                  <Typography variant="body2" style={{margin:"10px 20px"}}>{subtitle}</Typography>
+            <a href="{link}" >
+                <div className="main">
+                  <Paper className='categoryBlock'>
+                  {children}
+                  <Typography variant="h2" style={{position:"absolute",color:"#fff", margin:'2rem'}}>{title}</Typography>
+                  </Paper>
+                  <div  className="categoryContent">
+                    <Typography variant="subtitle1" className='categoryContentText'>{title}</Typography>
+                    <Typography variant="body2" style={{margin:"10px 20px"}}>{subtitle}</Typography>
+                  </div>
                 </div>
                 
-            </Link>
+            </a>
         </Grid>
   ); 
 }         
