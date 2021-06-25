2b5af6f4874ba233308e79cf83b22be724f6dea7
Daniil Zhdanov <yeezysmem@gmail.com>
2021-06-25 18:32:44 +0300

    Added breakpoint

--- src/common/HeineForm/content/Content.jsx
+++ src/common/HeineForm/content/Content.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Grid, Button, Typography, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  formContent: {
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: 50,
+    },
+  }
+}));
 
-export default function Content({ size = 6, article, children, image }) {
+export default function Content({ size = 12, sizeSm = 6, sizeMd = 6, article, children, image }) {
   const classes = useStyles();
   return (
-    <Grid item xs={size}>
+    <Grid item xs={size} sm={sizeSm} md={sizeMd} className={classes.formContent}>
       <div>
         <img src={image}></img>
         {children}
